7d9fc50a4e603c4ca282c3906b01d9de32f5986b
James Clark <james.clark@quantemplate.com>
2015-12-29 21:34:17 +0000

    add nestled react components

--- source/app.js
+++ source/app.js
@@ -1,4 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var reactElement = React.createElement('h1', { className: 'header' });
-ReactDOM.render(reactElement, document.getElementById('react-application'));
+var h1 = React.createElement('h1', { className: 'header', key: 'header' }, 'this is react');
+var p = React.createElement('p', { className: 'content', key: 'content' }, "And that's how it works.");
+var reactFragment = [h1, p];
+var section = React.createElement('section', { className: 'container' }, reactFragment);
+ReactDOM.render(section, document.getElementById('react-application'));
